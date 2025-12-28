70ceec7b4db89f585b5d55f25e5fd5db68aec75b
NeTenebrae <156097208+NeTenebraes@users.noreply.github.com>
2025-12-27 21:30:23 -0500

    Add echo_warn function for warning messages

--- setup.sh
+++ setup.sh
@@ -47,6 +47,7 @@ echo_msg() { echo -e "\n\033[1;34m🛡️ $1\033[0m"; }
 echo_ok() { echo -e "\033[1;32m✅ $1\033[0m"; }
 echo_skip(){ echo -e "\033[1;33m⏭️ $1\033[0m"; }
 echo_err() { echo -e "\033[0;31m❌ $1\033[0m" >&2; }
+echo_warn() { echo -e "\033[0;31m❌ $1\033[0m" >&2; }
 
 # Funciones Helper
 check_file_content() {
